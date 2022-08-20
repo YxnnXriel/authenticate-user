30fb520d53c3f664c610c49fa3e83308a868c252
yann ariel <yannariel13@gmail.com>
2022-08-20 21:35:04 +0000

    deuxieme commit

--- API/settings.py
+++ API/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*!i+9-hqh)&s9(4u65ps+urs&bgqawll=d3&s!it*lu%al4j40'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
